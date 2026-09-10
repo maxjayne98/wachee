5d9537a80b51ad3d2b4a715864c94d6491e6024b
maxjayne98 <36724807+maxjayne98@users.noreply.github.com>
2026-09-11 01:33:54 +0200

    refactor: remove inconsistent tailwind properies in range slider

--- src/components/RangeSlider.vue
+++ src/components/RangeSlider.vue
@@ -31,9 +31,15 @@ const maxPercent = computed(() => {
   return ((model.value.max - props.min) / (props.max - props.min)) * 100
 })
 
+// Native thumb centers travel from half a thumb inside either end of the input.
+const thumbPosition = (percent: number) =>
+  `calc(${percent}% + (0.5 - ${percent / 100}) * var(--thumb-size))`
+
+const minPosition = computed(() => thumbPosition(minPercent.value))
+const maxPosition = computed(() => thumbPosition(maxPercent.value))
 const rangeStyle = computed(() => ({
-  left: `${minPercent.value}%`,
-  width: `${maxPercent.value - minPercent.value}%`,
+  left: minPosition.value,
+  right: `calc(100% - ${maxPosition.value})`,
 }))
 
 function updateMin(value: number) {
@@ -52,25 +58,22 @@ function updateMax(value: number) {
 </script>
 
 <template>
-  <div class="group w-full select-none">
+  <div class="group w-full select-none [--thumb-size:--spacing(5)]">
     <div class="relative h-20">
-      <!-- Base track -->
-      <div class="absolute left-0 top-1/2 h-1.5 w-full -translate-y-1/2 rounded-full bg-gray-700" />
+      <div class="absolute left-0 top-1/2 h-1.5 w-full -translate-y-1/2 rounded-full bg-slate-800" />
 
-      <!-- Active range -->
       <div
-        class="absolute top-1/2 h-1.5 -translate-y-1/2 rounded-full bg-linear-to-r from-[#0E99F9] via-[#9761E8] to-[#FB8E51] shadow-[0_0_14px_rgba(151,97,232,0.18)]"
+        class="absolute top-1/2 h-1.5 -translate-y-1/2 rounded-full gradient-spectrum shadow-sm shadow-purple-500/30"
         :style="rangeStyle"
       />
 
-      <!-- Min thumb -->
       <input
         :value="model.min"
         :min="min"
         :max="max"
         :step="step"
         type="range"
-        class="pointer-events-none absolute inset-0 z-20 h-full w-full touch-none select-none appearance-none bg-transparent outline-none [&::-webkit-slider-runnable-track]:h-1.5 [&::-webkit-slider-runnable-track]:bg-transparent [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:size-5 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-[#F5F6FA] [&::-webkit-slider-thumb]:bg-[#D8E9FF] [&::-webkit-slider-thumb]:shadow-[0_0_0_7px_rgba(216,233,255,0.08)] [&::-webkit-slider-thumb]:transition [&::-webkit-slider-thumb]:duration-150 hover:[&::-webkit-slider-thumb]:scale-110 hover:[&::-webkit-slider-thumb]:shadow-[0_0_0_9px_rgba(151,97,232,0.12)] [&::-moz-range-track]:h-1.5 [&::-moz-range-track]:bg-transparent [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:size-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-[#F5F6FA] [&::-moz-range-thumb]:bg-[#D8E9FF] [&::-moz-range-thumb]:shadow-[0_0_0_7px_rgba(216,233,255,0.08)]"
+        class="pointer-events-none absolute inset-0 z-20 m-0 h-full w-full border-0 p-0 touch-none select-none appearance-none bg-transparent outline-none [&::-webkit-slider-runnable-track]:h-1.5 [&::-webkit-slider-runnable-track]:bg-transparent [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:-mt-1.75 [&::-webkit-slider-thumb]:box-border [&::-webkit-slider-thumb]:size-(--thumb-size) [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:bg-sky-100 [&::-webkit-slider-thumb]:shadow-sm hover:[&::-webkit-slider-thumb]:scale-110 hover:[&::-webkit-slider-thumb]:shadow-md hover:[&::-webkit-slider-thumb]:shadow-purple-500/30 [&::-moz-range-track]:h-1.5 [&::-moz-range-track]:bg-transparent [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:box-border [&::-moz-range-thumb]:size-(--thumb-size) [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:bg-sky-100 [&::-moz-range-thumb]:shadow-sm hover:[&::-moz-range-thumb]:scale-110 hover:[&::-moz-range-thumb]:shadow-md hover:[&::-moz-range-thumb]:shadow-purple-500/30"
         @input="updateMin(Number(($event.target as HTMLInputElement).value))"
       />
 
@@ -81,38 +84,36 @@ function updateMax(value: number) {
         :max="max"
         :step="step"
         type="range"
-        class="pointer-events-none absolute inset-0 z-30 h-full w-full touch-none select-none appearance-none bg-transparent outline-none [&::-webkit-slider-runnable-track]:h-1.5 [&::-webkit-slider-runnable-track]:bg-transparent [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:size-5 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-[#F5F6FA] [&::-webkit-slider-thumb]:bg-[#D8E9FF] [&::-webkit-slider-thumb]:shadow-[0_0_0_7px_rgba(216,233,255,0.08)] [&::-webkit-slider-thumb]:transition [&::-webkit-slider-thumb]:duration-150 hover:[&::-webkit-slider-thumb]:scale-110 hover:[&::-webkit-slider-thumb]:shadow-[0_0_0_9px_rgba(151,97,232,0.12)] [&::-moz-range-track]:h-1.5 [&::-moz-range-track]:bg-transparent [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:size-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-[#F5F6FA] [&::-moz-range-thumb]:bg-[#D8E9FF] [&::-moz-range-thumb]:shadow-[0_0_0_7px_rgba(216,233,255,0.08)]"
+        class="pointer-events-none absolute inset-0 z-30 m-0 h-full w-full border-0 p-0 touch-none select-none appearance-none bg-transparent outline-none [&::-webkit-slider-runnable-track]:h-1.5 [&::-webkit-slider-runnable-track]:bg-transparent [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:-mt-1.75 [&::-webkit-slider-thumb]:box-border [&::-webkit-slider-thumb]:size-(--thumb-size) [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:bg-sky-100 [&::-webkit-slider-thumb]:shadow-sm hover:[&::-webkit-slider-thumb]:scale-110 hover:[&::-webkit-slider-thumb]:shadow-md hover:[&::-webkit-slider-thumb]:shadow-purple-500/30 [&::-moz-range-track]:h-1.5 [&::-moz-range-track]:bg-transparent [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:box-border [&::-moz-range-thumb]:size-(--thumb-size) [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-white [&::-moz-range-thumb]:bg-sky-100 [&::-moz-range-thumb]:shadow-sm hover:[&::-moz-range-thumb]:scale-110 hover:[&::-moz-range-thumb]:shadow-md hover:[&::-moz-range-thumb]:shadow-purple-500/30"
         @input="updateMax(Number(($event.target as HTMLInputElement).value))"
       />
 
-      <!-- Min tooltip -->
       <div
-        class="pointer-events-none absolute bottom-[calc(50%+14px)] z-40 -translate-x-1/2 select-none opacity-60 scale-95 transition-all duration-200 ease-out group-hover:opacity-100 group-hover:scale-100 group-focus-within:opacity-100 group-focus-within:scale-100"
-        :style="{ left: `${minPercent}%` }"
+        class="pointer-events-none absolute bottom-[calc(50%+14px)] z-40 -translate-x-1/2 select-none opacity-60 scale-95 transition-[opacity,scale] duration-200 ease-out group-hover:opacity-100 group-hover:scale-100 group-focus-within:opacity-100 group-focus-within:scale-100"
+        :style="{ left: minPosition }"
       >
         <div
-          class="relative rounded-md border border-[#282D3F] bg-[#151823] px-2.5 py-1 text-xs font-medium text-[#8C95A7] shadow-sm transition-all duration-200 ease-out group-hover:border-white group-hover:bg-[#F5F6FA] group-hover:text-[#07080D] group-hover:font-bold group-hover:shadow-[0_6px_20px_rgba(0,0,0,0.35)]"
+          class="relative rounded-md border border-slate-700 bg-slate-900 px-2.5 py-1 text-xs font-medium text-slate-400 shadow-sm transition-all duration-200 ease-out group-hover:border-white group-hover:bg-slate-100 group-hover:text-slate-950 group-hover:font-bold group-hover:shadow-lg group-hover:shadow-black/40"
         >
           {{ model.min }}
 
           <span
-            class="absolute left-1/2 top-full size-2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-b border-r border-[#282D3F] bg-[#151823] transition-all duration-200 ease-out group-hover:border-white group-hover:bg-[#F5F6FA]"
+            class="absolute left-1/2 top-full size-2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-b border-r border-slate-700 bg-slate-900 transition-all duration-200 ease-out group-hover:border-white group-hover:bg-slate-100"
           />
         </div>
       </div>
 
-      <!-- Max tooltip -->
       <div
-        class="pointer-events-none absolute bottom-[calc(50%+14px)] z-40 -translate-x-1/2 select-none opacity-60 scale-95 transition-all duration-200 ease-out group-hover:opacity-100 group-hover:scale-100 group-focus-within:opacity-100 group-focus-within:scale-100"
-        :style="{ left: `${maxPercent}%` }"
+        class="pointer-events-none absolute bottom-[calc(50%+14px)] z-40 -translate-x-1/2 select-none opacity-60 scale-95 transition-[opacity,scale] duration-200 ease-out group-hover:opacity-100 group-hover:scale-100 group-focus-within:opacity-100 group-focus-within:scale-100"
+        :style="{ left: maxPosition }"
       >
         <div
-          class="relative rounded-md border border-[#282D3F] bg-[#151823] px-2.5 py-1 text-xs font-medium text-[#8C95A7] shadow-sm transition-all duration-200 ease-out group-hover:border-white group-hover:bg-[#F5F6FA] group-hover:text-[#07080D] group-hover:font-bold group-hover:shadow-[0_6px_20px_rgba(0,0,0,0.35)]"
+          class="relative rounded-md border border-slate-700 bg-slate-900 px-2.5 py-1 text-xs font-medium text-slate-400 shadow-sm transition-all duration-200 ease-out group-hover:border-white group-hover:bg-slate-100 group-hover:text-slate-950 group-hover:font-bold group-hover:shadow-lg group-hover:shadow-black/40"
         >
           {{ model.max }}
 
           <span
-            class="absolute left-1/2 top-full size-2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-b border-r border-[#282D3F] bg-[#151823] transition-all duration-200 ease-out group-hover:border-white group-hover:bg-[#F5F6FA]"
+            class="absolute left-1/2 top-full size-2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-b border-r border-slate-700 bg-slate-900 transition-all duration-200 ease-out group-hover:border-white group-hover:bg-slate-100"
           />
         </div>
       </div>
